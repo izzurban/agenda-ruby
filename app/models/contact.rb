887d3a356cc9cd59f5009eb817c410a37b9762d0
Thiago Nabruzzi <thiago.nabruzzi@fatec.sp.gov.br>
2020-02-26 11:06:43 -0300

    Validacao tel contato

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,8 +2,11 @@ class Contact < ApplicationRecord
   
   mount_uploader :image, ImageUploader
 
+  VALID_PHONE_REGEX = /(\(?\d{2}\)?\s)?(\d{4,5}\-?\d{4,5})/i
+
     validates :name, presence: true, length: { maximum: 50 }
-    validates :phone, presence: true
+    validates :phone, presence: true, length: { maximum: 15, minimum: 10},
+                                      format: { with: VALID_PHONE_REGEX }
   
     belongs_to :user
   
